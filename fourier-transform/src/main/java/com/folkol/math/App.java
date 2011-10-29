2b4144c061983e4bb18b8216f3bb09d608073b10
Mattias Johansson <mattias.johansson@atex.com>
2011-10-29 18:38:41 +0200

    Created fourier project

---
+++ fourier-transform/src/main/java/com/folkol/math/App.java
@@ -0,0 +1,13 @@
+package com.folkol.math;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
