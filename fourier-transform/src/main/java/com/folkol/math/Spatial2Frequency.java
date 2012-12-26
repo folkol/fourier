c2c978d909786011c79a98b7e393403566171612
Mattias Johansson <mattias.johansson@atex.com>
2012-12-26 23:44:25 +0100

    Added gitignore

--- fourier-transform/src/main/java/com/folkol/math/Spatial2Frequency.java
+++ fourier-transform/src/main/java/com/folkol/math/Spatial2Frequency.java
@@ -1,13 +1,29 @@
 package com.folkol.math;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 /**
  * Hello world!
  *
  */
-public class Spatial2Frequency 
+public class Spatial2Frequency
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File(args[0]));
+            Graphics g = img.getGraphics();
+            g.drawLine(0, 0, 100, 100);
+            ImageIO.write(img, "png", new File(args[1]));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
